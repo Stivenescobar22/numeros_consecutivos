146d4c81487188a367f81d774d657ebc4a9266fc
Stivenescobar22 <estibenes@gmail.com>
2019-09-15 18:41:35 -0500

    programa que muestra los numeros consecutivos de un cantidad asignada

---
+++ NumConsecutivos/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace NumConsecutivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.txtcantidad = new System.Windows.Forms.TextBox();
+            this.btnmostrar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.lsbconsecutivos = new System.Windows.Forms.ListBox();
+            this.lblcantidad = new System.Windows.Forms.Label();
+            this.lblmensaje = new System.Windows.Forms.Label();
+            this.btnborrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(27, 9);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(282, 20);
+            this.lbltitulo.TabIndex = 0;
+            this.lbltitulo.Text = "Muestra de numeros consecutivos";
+            // 
+            // txtcantidad
+            // 
+            this.txtcantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcantidad.Location = new System.Drawing.Point(30, 99);
+            this.txtcantidad.Name = "txtcantidad";
+            this.txtcantidad.Size = new System.Drawing.Size(100, 22);
+            this.txtcantidad.TabIndex = 1;
+            this.txtcantidad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtcantidad_KeyPress);
+            // 
+            // btnmostrar
+            // 
+            this.btnmostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmostrar.Location = new System.Drawing.Point(160, 96);
+            this.btnmostrar.Name = "btnmostrar";
+            this.btnmostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnmostrar.TabIndex = 2;
+            this.btnmostrar.Text = "Mostrar";
+            this.btnmostrar.UseVisualStyleBackColor = true;
+            this.btnmostrar.Click += new System.EventHandler(this.btnmostrar_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsalir.Location = new System.Drawing.Point(234, 267);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(75, 23);
+            this.btnsalir.TabIndex = 3;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // lsbconsecutivos
+            // 
+            this.lsbconsecutivos.FormattingEnabled = true;
+            this.lsbconsecutivos.Location = new System.Drawing.Point(30, 143);
+            this.lsbconsecutivos.Name = "lsbconsecutivos";
+            this.lsbconsecutivos.Size = new System.Drawing.Size(120, 147);
+            this.lsbconsecutivos.TabIndex = 4;
+            // 
+            // lblcantidad
+            // 
+            this.lblcantidad.AutoSize = true;
+            this.lblcantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcantidad.Location = new System.Drawing.Point(40, 80);
+            this.lblcantidad.Name = "lblcantidad";
+            this.lblcantidad.Size = new System.Drawing.Size(70, 16);
+            this.lblcantidad.TabIndex = 5;
+            this.lblcantidad.Text = "Cantidad";
+            // 
+            // lblmensaje
+            // 
+            this.lblmensaje.AutoSize = true;
+            this.lblmensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmensaje.Location = new System.Drawing.Point(185, 143);
+            this.lblmensaje.Name = "lblmensaje";
+            this.lblmensaje.Size = new System.Drawing.Size(62, 15);
+            this.lblmensaje.TabIndex = 6;
+            this.lblmensaje.Text = "mensaje";
+            this.lblmensaje.Visible = false;
+            // 
+            // btnborrar
+            // 
+            this.btnborrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnborrar.Location = new System.Drawing.Point(247, 96);
+            this.btnborrar.Name = "btnborrar";
+            this.btnborrar.Size = new System.Drawing.Size(75, 23);
+            this.btnborrar.TabIndex = 7;
+            this.btnborrar.Text = "Borrar";
+            this.btnborrar.UseVisualStyleBackColor = true;
+            this.btnborrar.Click += new System.EventHandler(this.btnborrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 302);
+            this.Controls.Add(this.btnborrar);
+            this.Controls.Add(this.lblmensaje);
+            this.Controls.Add(this.lblcantidad);
+            this.Controls.Add(this.lsbconsecutivos);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnmostrar);
+            this.Controls.Add(this.txtcantidad);
+            this.Controls.Add(this.lbltitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.TextBox txtcantidad;
+        private System.Windows.Forms.Button btnmostrar;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.ListBox lsbconsecutivos;
+        private System.Windows.Forms.Label lblcantidad;
+        private System.Windows.Forms.Label lblmensaje;
+        private System.Windows.Forms.Button btnborrar;
+    }
+}
+
---
+++ NumConsecutivos/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumConsecutivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnmostrar_Click(object sender, EventArgs e)
+        {
+            int cantidad = int.Parse(txtcantidad.Text);
+            int contador = 0;
+            int resta = 0;
+
+            lsbconsecutivos.Items.Clear();
+
+            while (contador <= cantidad){
+
+                if (contador <= cantidad){
+
+                    lsbconsecutivos.Items.Add(contador.ToString());
+
+                }
+
+                
+                lblmensaje.Text = " Muestra Finalizada";
+                lblmensaje.Visible = true;
+                contador++;
+                txtcantidad.Text = "";
+                txtcantidad.Focus();
+            }
+
+            
+        }
+
+       
+
+        private void txtcantidad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar))
+            {
+
+                e.Handled = false;
+            }
+            else if (char.IsControl(e.KeyChar))
+            {
+
+                e.Handled = false;
+            }
+            else if (char.IsSeparator(e.KeyChar))
+            {
+
+                e.Handled = false;
+            }
+            else
+            {
+
+                MessageBox.Show("Solo se pueden numeros"
+                    , "Mensaje de advertencia"
+                    , MessageBoxButtons.OK
+                    , MessageBoxIcon.Error);
+
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void btnborrar_Click(object sender, EventArgs e)
+        {
+            lsbconsecutivos.Items.Clear();
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
